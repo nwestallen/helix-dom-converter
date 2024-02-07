4f040161c2d6efcd456f473bd1d251ef1aae6f88
Nick <nwestallen@gmail.com>
2024-02-07 10:32:52 -0800

    functioning input and output textareas

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -5,30 +5,29 @@
             ["react-dom/client" :as rdom]
             [app.html :as html]))
 
-;; define components using the `defnc` macro
-(defnc greeting
-  "A component which greets a user."
-  [{:keys [name]}]
-  ;; use helix.dom to create DOM elements
-  (d/div {:class-name "px-5"}"Hello, " (d/strong name) "!"))
+(def input-format "border border-black p-2 w-full h-5/6 resize-none")
 
 (defnc app []
   {:helix/features {:fast-refresh true}}
-  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
+  (let [[rawinput set-rawinput] (hooks/use-state "")]
     (d/div
-     (d/h1 {:class-name "text-4xl font-bold p-5"} "Welcome!")
-     ;; create elements out of components
-     ($ greeting {:name (:name state)})
-     (d/input {:class-name "mx-5 my-1 border border-black"
-               :value (:name state)
-               :on-change #(set-state assoc :name (.. % -target -value))})
-      (d/p (str (html/hick:html->dom "<div class=\"foo\">
-   <button class=\"asdf\" aria-role=\"jkl\">qwfp</button>
-   <input value=\"1\">
-   <span><a href=\"/bar\">Baz</a></span>
- </div>" ))))))
+     (d/h1 {:class-name "text-4xl font-bold m-4"} "HTML to Helix-Dom")
+     (d/div {:class-name "flex w-5/6 border border-black h-[75vh] m-4"}
+            (d/div {:class-name "p-2 w-1/2"}
+             (d/h2 {:class-name "font-bold"}"HTML")
+             (d/textarea {:class-name input-format
+                          :value rawinput
+                          :on-change #(set-rawinput (.. % -target -value))}))
+            (d/div {:class-name "p-2 w-1/2"}
+             (d/h2 {:class-name "font-bold"} "Helix-Dom")
+             (d/textarea {:class-name input-format
+                          :value (str (html/hick:html->dom rawinput))}))))))
 
-;; start your app with your favorite React renderer
+;;TODO format output text; newline & indent nested tags/components/parentheses
+;;TODO modify html/hick:html->dom to handle html comments
+;;TODO enable users to select helix-dom abbreviation/prefix e.g. "d/" instead of "dom/"
+;;TODO map keywords that are different from html e.g. "class" -> ":class-name"
+
 (defn ^:export init []
-(let [root (rdom/createRoot (js/document.getElementById "root"))]
-(.render root ($ app))))
+  (let [root (rdom/createRoot (js/document.getElementById "root"))]
+    (.render root ($ app))))
