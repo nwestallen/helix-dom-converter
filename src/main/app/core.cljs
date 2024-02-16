8a28194ca20010c71219f346c6a9c8affc3cd748
Nick <nwestallen@gmail.com>
2024-02-16 14:43:28 -0800

    line breaks in output, onChange fix

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -7,6 +7,7 @@
             ["@uiw/react-codemirror" :default CodeMirror]
             ["@codemirror/lang-html" :refer (html)]
             ["@nextjournal/lang-clojure" :refer (clojure)]
+            [clojure.string :as s]
             ))
 
 (def input-format "border border-black p-2 w-full h-[96%] resize-none rounded-md")
@@ -15,7 +16,13 @@
   <input value=\"1\">
   <span><a href=\"/bar\">Baz</a></span>
   </div>")
-(comment )
+(defn autowrap [s]
+  (apply str
+   (first s)
+   (s/replace (apply str (rest s)) #"\(" "\n (")
+   )
+  )
+
 (defnc app []
   {:helix/features {:fast-refresh true}}
   (let [[rawinput set-rawinput] (hooks/use-state default-input)
@@ -23,18 +30,18 @@
     (d/div {:class-name "bg-stone-300 h-screen"}
       (d/h1 {:class-name "text-4xl text-slate-50 font-bold p-7 bg-stone-700 drop-shadow-lg"} "HTML to Helix-Dom")
      (d/p {:class-name "mx-6 mt-2 font-bold"}"helix.dom alias")
-     (d/input {:class-name "mx-6" :value domalias :on-change #(set-domalias (.. % -target -value))})
+     (d/input {:id "alias-input":class-name "mx-6" :value domalias :on-change #(set-domalias (.. % -target -value))})
      (d/div {:class-name "flex w-5/6 h-[75vh] m-4"}
             (d/div {:class-name "p-2 w-1/2 bg-blue-400 m-2 rounded-md drop-shadow-xl"}
              (d/h2 {:class-name "font-bold text-xl"}"HTML")
              ($ CodeMirror {:class-name input-format
                           :value rawinput
                           :extensions (html)
-                          :on-change #(set-rawinput (.. % -target -value))}))
+                          :onChange (fn [val] ((set-rawinput val)))}))
             (d/div {:class-name "p-2 w-1/2 bg-teal-400 m-2 rounded-md drop-shadow-xl"}
              (d/h2 {:class-name "font-bold text-xl"} "Helix-Dom")
              ($ CodeMirror {:class-name input-format
-                          :value (str (html/hick:html->dom rawinput :alias domalias))
+                          :value (autowrap (str (html/hick:html->dom rawinput :alias domalias)))
                           :extensions (clojure)}))))))
 
 ;;TODO format output text; newline & indent nested tags/components/parentheses
